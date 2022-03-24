9ca64911d637a643588322df7c3db8680679a0aa
lena0911 <lenagolovkina09@gmail.com>
2022-03-24 16:38:47 +0300

    Задание по базовому синтаксису

--- src/BasicSyntax.js
+++ src/BasicSyntax.js
@@ -1,8 +1,47 @@
+function fn(a) {
+    if (a == 'M') {
+        return 1000;
+    }
+    if (a == 'D') {
+        return 500;
+    }
+    if (a == 'C') {
+        return 100;
+    }
+    if (a == 'L') {
+        return 50;
+    }
+    if (a == 'X') {
+        return 10;
+    }
+    if (a == 'V') {
+        return 5;
+    }
+    if (a == 'I') {
+        return 1;
+    }
+    return 0;
+}
 export function romanToInteger(str) {
     let result = 0;
+    let i = 0,
+        left,
+        right;
     /*
     Напишите код, преобразующий число из римской записи в арабскую.
     На вход вы получите строку, ответом должно быть число.
      */
+    while (i + 1 <= str.length) {
+        left = fn(str[i]);
+        right = fn(str[i + 1]);
+        if (left >= right) {
+            result += left;
+        } else {
+            result += right - left;
+            i++;
+        }
+        i++;
+        left = right;
+    }
     return result;
 }
